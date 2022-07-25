b59f5e0cd5e4a7934967915330b8e917d572d286
Darchuk Sergey <Darchuk.Sergey@gmail.com>
2022-07-25 19:41:20 +0300

    added accordion

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 $(function () {
-    $('.plan__accordion-title').on('click', function () {
-
-        $('.plan__accordion').removeClass('plan__accordion--active');
-        $(this).parent().addClass('plan__accordion--active');
+    $(".plan__accordion-title.active").next().slideDown();
+    $(".plan__accordion-title").on("click", function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass("active").next().slideUp();
+        } else {
+            $(".plan__accordion-title.active").removeClass("active").next(".plan__accordion-items").slideUp();
+            $(this).addClass('active').next('.plan__accordion-items').slideDown();
+        }
     });
-    $('.price__accordion-title').on('click', function () {
 
-        $('.price__accordion-inner').removeClass('price__accordion--active');
-        $(this).parent().addClass('price__accordion--active');
+    $(".price__accordion-title.active").next().slideDown();
+    $(".price__accordion-title").on("click", function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass("active").next().slideUp();
+        } else {
+            $(".price__accordion-title.active").removeClass("active").next(".price__accordion-items").slideUp();
+            $(this).addClass('active').next('.price__accordion-items').slideDown();
+        }
     });
+
 
     $('.carousel__inner').each(function () {
         var $status = $('.carousel__buttons-counter');
